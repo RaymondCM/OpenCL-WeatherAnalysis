70722fb05e4c2e1eb686a23e7c7b576afc8badf8
RaymondKirk <ray.tunstill@live.co.uk>
2017-03-24 16:48:47 +0000

    Added seperate parse file

---
+++ Parser.hpp
@@ -0,0 +1,53 @@
+namespace Parse {
+	template<typename T>
+	bool File(std::string file_path, std::vector<T>& dest) {
+		//Open input stream to file
+		std::ifstream input_file(file_path);
+
+		//Seek to the end of the file and get the position of the last char
+		input_file.seekg(0, std::ios_base::end);
+		std::size_t size = input_file.tellg();
+
+		//Seek back to the start of the file
+		input_file.seekg(0, std::ios_base::beg);
+
+		//Read into char buffer with size of file
+		char * file_contents = new char[size];
+		input_file.read(&file_contents[0], size);
+
+		//Close the file
+		input_file.close();
+
+		//Parse the file by keeping track of the last space before \n
+		long space_pos = 0;
+
+		//Final vector of values
+
+		for (long i = 0; i < size; ++i) {
+			char c = file_contents[i];
+
+			if (c == ' ') {
+				//+1 to i to itterate after last space
+				space_pos = i + 1;
+			}
+			else if (c == '\n') {
+				int len = i - space_pos, index = 0;
+
+				//Allocate buffer for word between space_pos and \n
+				char * word = new char[len];
+				word[len] = '\0';
+
+				//Get every char between last space and \n
+				for (int j = space_pos; j < i; j++) {
+					word[index++] = file_contents[j];
+				}
+
+				//Parse word to double (higher precison)
+				dest.push_back(::atof(word));
+			}
+		};
+
+		return true;
+	};
+
+}
--- main.cpp
+++ main.cpp
@@ -16,57 +16,8 @@
 #endif
 
 #include "Utils.hpp"
+#include "Parser.hpp"
 
-template<typename T>
-bool ParseFile(std::string file_path, std::vector<T>& dest) {
-	//Open input stream to file
-	std::ifstream input_file(file_path);
-
-	//Seek to the end of the file and get the position of the last char
-	input_file.seekg(0, std::ios_base::end);
-	std::size_t size = input_file.tellg();
-
-	//Seek back to the start of the file
-	input_file.seekg(0, std::ios_base::beg);
-
-	//Read into char buffer with size of file
-	char * file_contents = new char[size];
-	input_file.read(&file_contents[0], size);
-
-	//Close the file
-	input_file.close();
-
-	//Parse the file by keeping track of the last space before \n
-	long space_pos = 0;
-
-	//Final vector of values
-
-	for (long i = 0; i < size; ++i) {
-		char c = file_contents[i];
-
-		if (c == ' ') {
-			//+1 to i to itterate after last space
-			space_pos = i + 1;
-		}
-		else if (c == '\n') {
-			int len = i - space_pos, index = 0;
-
-			//Allocate buffer for word between space_pos and \n
-			char * word = new char[len];
-			word[len] = '\0';
-
-			//Get every char between last space and \n
-			for (int j = space_pos; j < i; j++) {
-				word[index++] = file_contents[j];
-			}
-
-			//Parse word to double (higher precison)
-			dest.push_back(strtod(word, NULL));
-		}
-	};
-
-	return true;
-};
 
 int main(int argc, char **argv) {
 	//Get user options from command line
@@ -89,9 +40,9 @@ int main(int argc, char **argv) {
 	std::string file_path = root + "/data/temp_lincolnshire.txt";
 	std::string kernels_path = root + "/opencl/kernels.cl";
 
-	typedef int temptype;
+	typedef float temptype;
 	std::vector<temptype> temperature;
-	ParseFile(file_path, temperature);
+	Parse::File(file_path, temperature);
 
 	try {
 		//Select both platform and device from user options 
