1afcac21a911d5327a77d77a2688e774af0f24d9
RaymondKirk <ray.tunstill@live.co.uk>
2017-03-28 21:20:46 +0100

    Updated parser to make more assumptions about data structure

--- Parser.hpp
+++ Parser.hpp
@@ -1,13 +1,22 @@
 #include <fstream>
+#include "SimpleTimer.hpp"
 
 namespace Parse {
-	template<typename T>
-	std::vector<T> File(std::string file_path) {
-		//Open input stream to file
-		std::ifstream input_file(file_path, std::ios::in | std::ios::binary);
+	void NumericData(char data[], std::vector<int>& dest) {
+		dest.push_back(::atoi(data));
+	};
 
+	void NumericData(char data[], std::vector<float>& dest) {
+		dest.push_back(::atof(data));
+	};
+
+	template<typename T>
+	void FileEOL(std::string file_path, std::vector<T>& destination) {
+		//Open input stream to file
 		//Seek to the end of the file and get the position of the last char
-		input_file.seekg(0, std::ios_base::end);
+		std::ifstream input_file(file_path, std::ios::in | std::ios::binary | std::ios::ate);
+
+		//Get Size of File
 		std::size_t size = input_file.tellg();
 
 		//Seek back to the start of the file
@@ -19,37 +28,33 @@ namespace Parse {
 
 		//Close the file
 		input_file.close();
+		int c = 0;
+		for (unsigned int i = 0; i < size; ++i) {
+			if (file_contents[i] == '\n') {
+				char word[7] = {"     \0"};
+				int j = i, counter = 6;
 
-		//Parse the file by keeping track of the last space before \n
-		long space_pos = 0;
+				while(file_contents[j] != ' ')
+					*(word + counter--) = file_contents[j--];
 
-		//Final vector of values
-		std::vector<T> dest;
-
-		for (long i = 0; i < size; ++i) {
-			char c = file_contents[i];
-
-			if (c == ' ') {
-				//+1 to i to itterate after last space
-				space_pos = i + 1;
-			} else if (c == '\n') {
-				int len = i - space_pos, index = 0;
-
-				//Allocate buffer for word between space_pos and \n
-				char * word = new char[len];
-				word[len] = '\0';
-
-				//Get every char between last space and \n
-				for (int j = space_pos; j < i; j++) {
-					word[index++] = file_contents[j];
-				}
-
-				//Parse word to double (higher precison)
-				dest.push_back(::atof(word));
+				c++;
+				Parse::NumericData(word, destination);
 			}
 		};
-
-		return dest;
 	};
 
+	template<typename T>
+	void File(std::string file_path, std::vector<T>& destination) {
+        SimpleTimer t;
+		t.Tic();
+		Parse::FileEOL(file_path, destination);
+        std::cout << "File Parsed in " << t.Toc() / 1000000 << "ms" << std::endl;
+	};
+
+	template<typename T>
+	std::vector<T> File(std::string file_path) {
+		std::vector<T> data;
+		File(file_path, data);
+		return data;
+	};
 }
