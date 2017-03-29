874d554d09802041f0c64e5074601e1d0276dec2
Raymond Kirk <ray.tunstill@live.co.uk>
2017-03-29 21:53:30 +0100

    Added recursive kernels reducing local buffers to 1
    Fixed buffer sizes for output and input.
    Added enqueue kernel functions for recursivly calling until the buffer has a size of zero.
    Added kernel name generator.
    Added timer to main for total time.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "SimpleTimer.hpp"
 
 #include "WeatherAnalysis.hpp"
 #include "Parser.hpp"
 
 int main(int argc, char **argv) {
-	std::string root(".");
+    SimpleTimer t;
+    t.Tic();
+    std::string root(".");
 
-	#ifdef PROJECT_ROOT
-		root = PROJECT_ROOT;
-	#endif
+#ifdef PROJECT_ROOT
+    root = PROJECT_ROOT;
+#endif
 
-	std::string file_path = root + "/data/temp_lincolnshire_short.txt";
-	std::string kernels_path = root + "/opencl/kernels.cl";
+    std::string file_path = root + "/data/temp_lincolnshire.txt";
+    std::string kernels_path = root + "/opencl/kernels.cl";
 
-	typedef int T;
-	std::vector<T> data = Parse::File<T>(file_path);
+    typedef int T;
+    std::vector<T> data = Parse::File<T>(file_path);
     std::cout << "Size: " << data.size() << ", Last: " << data.back() << '\n' << std::endl;
 
     WeatherAnalysis<T> world(data);
-	world.CmdParser(argc, argv);
-	world.Initialise(kernels_path);
+    world.CmdParser(argc, argv);
+    world.Initialise(kernels_path);
 
     world.Configure(32, 0);
-    world.SetVerboseKernel(true);
+    world.SetVerboseKernel(false);
     world.UsePreferredKernelOptions(false);
-    world.PrintKernelProfilingData(false);
+    world.PrintKernelProfilingData(true);
+    world.SetKernelWorkGroupRecursion(false);
 
-	world.PadData();
+    world.PadData();
     world.PrintBaselineResults();
     world.WriteDataToDevice();
 
+    world.Min();
+    world.Max();
+    //world.Sum();
+    world.Average();
+    world.StdDeviation();
     world.Sort();
 
     world.PrintResults();
 
-	char c;
-	std::cin >> c;
 
-	return 0;
+    std::cout << "Program terminated in: " << std::fixed << t.Toc() / 1000000 << "ms" << std::endl;
+    char c;
+    std::cin >> c;
+
+    return 0;
 }
